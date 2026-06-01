694cbd69d38c861c79d8e48535418f6856f7dde7
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-01 18:00:16 -0400

    Fix function to work

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,6 @@
 const repeatString = function (string, num) {
-    let newString;
-    for (let i = 0; i < num; i++) {
+    let newString = string;
+    for (let i = 0; i < num - 1; i++) {
         newString += string;
         console.log(newString);
     }
