cb01d673062b8b5b9e3722ea5b80c6fac4e23d26
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-01 17:36:49 -0400

    Complete exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,12 +1,25 @@
 const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    if (num === 0) {
+        return "";
+    }
+
     let newString = string;
+
     for (let i = 0; i < num - 1; i++) {
         newString += string;
         console.log(newString);
     }
     return newString;
 
+
+
+
 };
 
+// Make string repeat 0 times when 0 is the number inputed 
+// run the debugger
 // Do not edit below this line
 module.exports = repeatString;
