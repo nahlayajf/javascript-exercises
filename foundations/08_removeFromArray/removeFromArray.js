a508dc20f9426cfa23ec994e3ef4b8e807baeeab
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-05 17:47:32 -0400

    Make function remove multiple of same value

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,11 +1,21 @@
 const removeFromArray = function (arr, ...items) {
+    let i = 0;
+    while (i < items.length) {
+        console.log(items)
+        console.log(arr)
 
-    console.log(items);
-    console.log(arr);
-    for (let i = 0; i < items.length; i++) {
-        let indexItem = arr.indexOf(items[i]);
-        console.log(arr.splice(indexItem, 1));
+        if (arr.includes(items[i])) {
+
+            let indexItem = arr.indexOf(items[i]);
+            arr.splice(indexItem, 1);
+        }
+        else {
+            i++
+        }
+
+
     }
+
     return arr;
 };
 
