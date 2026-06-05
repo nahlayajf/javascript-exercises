1719bef4c129082a50fce62f2427dfa6162c76b2
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-05 17:08:07 -0400

    Make function remove multiple values

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+
+    console.log(items);
+    console.log(arr);
+    for (let i = 0; i < items.length; i++) {
+        let indexItem = arr.indexOf(items[i]);
+        console.log(arr.splice(indexItem, 1));
+    }
+    return arr;
 };
 
 // Do not edit below this line
