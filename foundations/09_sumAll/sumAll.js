1b3c747abece8bd9bacd37db279802ab3a196b3e
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-05 18:18:25 -0400

    WIP: Edit function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let range = Math.abs(num1 - num2);
+    let startNum = Math.min(num1, num2)
 
-};
+    for (let i = startNum; i < range; i++) {
+        startNum += i;
+    }
 
+    let total = startNum;
+    return total
+}
+// Todo: Fix debuggerx
 // Do not edit below this line
 module.exports = sumAll;
+
