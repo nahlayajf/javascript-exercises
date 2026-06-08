8e985f292477b74b039239596fe92a43429c6a77
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-06-08 16:36:48 -0400

    Complete convert to celsius function

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (tempF) {
+  let tempInCel = (tempF - 32) * 5 / 9;
+  return Math.round(tempInCel * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
 };
 
 // Do not edit below this line
